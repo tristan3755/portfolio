2d760649020b7fe998e963d47d63f1a5ee6aa96b
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-01-21 17:58:04 +0100

    portfolio

--- javascript.js
+++ javascript.js
@@ -3,3 +3,16 @@ document.documentElement.addEventListener('mousemove',(e)=>{
     document.documentElement.style.setProperty('--x',e.clientX+"px")
     document.documentElement.style.setProperty('--y',e.clientY+"px")
 })
+
+window.addEventListener('scroll',(event)=>{
+
+    let valeur =scrollY/4;
+
+    let descriptionCompetence=document.querySelector('.descriptionCompetence')
+
+    descriptionCompetence.style.bottom= valeur * + 0.5 + "px"
+
+    let imageCompetence=document.querySelector('.html5')
+    imageCompetence.style.top= valeur * + 0.2 +"px"
+
+})
