04b532f8014ea93312ce38b3f96db5849bf0b861
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-03-14 00:27:58 +0100

    portfolio

--- email.php
+++ email.php
@@ -1,17 +1,18 @@
 <?php
 
+
 if(!empty($_POST)){
+
 
 
 $mess = ''.$_POST['prenom'].' '.$_POST['nom'].' , '.$_POST['objet'].' : '.$_POST['mail'].'     '.'from :'. $_POST['email'];
 
 $mail = mail('tristan3755@gmail.com','Envoi depuis la page Contact de mon portfolio',$mess);
 
+
+
 }
 
 header('Location:./index.html');
 exit;
-
-
-
 ?>
